3f551a92712eb81c9b199c306cf88dd46f38ec7c
S.Mozafari <S.Mozafari@MOZAFFARI>
2021-05-24 16:29:37 +0430

    fix style layout

--- Semicolon/Areas/User/Controllers/Post.cs
+++ Semicolon/Areas/User/Controllers/Post.cs
@@ -1,11 +1,21 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Web.Areas.User.Controllers
 {
-    public class Post
+    public class Post: UserBaseController
     {
+        public Post()
+        {
+
+        }
+
+        public async Task<IActionResult> Create()
+        {
+            return View();
+        }
     }
 }
